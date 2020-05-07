153f2ea1f445f193862eb02377e0901dfc8e8e66
LiubovShakhova <63476452+LiubovShakhova@users.noreply.github.com>
2020-05-07 16:26:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -105,22 +105,25 @@ function checkAuth() {
 }
 
 function createCardRestaurant(restaurant) {
-    console.log(restaurant);
+    const { image,
+      kitchen,name,
+      price,stars,
+      products,time_of_delivery } = restaurant;
 
     const card = `
       <a class="card card-restaurant">
-              <img src="img/palki-skalki/preview.jpg" alt="image" class="card-image"/>
+              <img src="${image}" alt="image" class="card-image"/>
               <div class="card-text">
                 <div class="card-heading">
-                  <h3 class="card-title">Палки скалки</h3>
-                  <span class="card-tag tag">55 мин</span>
+                  <h3 class="card-title">${name}</h3>
+                  <span class="card-tag tag">${time_of_delivery} мин</span>
                 </div>
                 <div class="card-info">
                   <div class="rating">
-                    4.5
+                    ${stars}
                   </div>
-                  <div class="price">От 500 ₽</div>
-                  <div class="category">Пицца</div>
+                  <div class="price">От ${price} ₽</div>
+                  <div class="category">${kitchen}</div>
                 </div>
               </div>
             </a>   
